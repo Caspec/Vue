120d857486597c9bbcccf3a02f1bb7882133d52c
Caspec <quii2000@yahoo.dk>
2020-04-15 13:47:40 +0200

    bootstrap vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
     <h1>home test</h1>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
 
+    <b-container fluid="md">
+  <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+</b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'Home',
-  components: {
-
-  }
-}
+  name: "Home",
+  components: {}
+};
 </script>
